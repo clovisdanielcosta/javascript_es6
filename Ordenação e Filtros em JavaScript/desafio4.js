29245c21da5f404dd9eb7c64e5e0e7f195cd4fa2
clovisdanielcosta <clovisdanielcosta@gmail.com>
2020-07-31 11:53:09 -0300

    Refatoring Challenge 4 from course Ordenação e Filtros em JavaScript by DIO

--- Ordenação e Filtros em JavaScript/desafio4.js
+++ Ordenação e Filtros em JavaScript/desafio4.js
@@ -3,32 +3,20 @@ let cases = parseInt(gets()); //Recebe quantidade de casos
 
 for (let i = 1; i <= cases; i++) {
 
-    const stack = []; //Cria a fila 
-    const stackOrdered = []; //Novo array para deixar os valores ordenados
     let stillInPosition = 0; //Conta os não alterados
     let numCustomers = parseInt(gets());
-    let arriving = (gets()).split(" "); //Recebe os clientes
-    //Carrega os valores nos novos arrays de fila
-    for (let i = 0; i < numCustomers; i++) {
-        stack.push(parseInt(arriving[i]));
-        stackOrdered.push(parseInt(stack[i]));              
-    };
-
-    //Deixando os tipos iguais
-    stack.map((stack) => stack);
+    let arriving = (gets()).split(" ").map((arriving) => arriving); //Recebe os clientes
+    //let arr1 = arriving.map((arriving) => arriving);
+    let stack = arriving.map((arriving) => arriving).sort(sortNum);
 
     //Ordenando em ordem descrescente e númerica
     function sortNum(a, b){
         return (b - a) //Função auxiliar para o array poder serja ordenado numericamente.
     }
-    stackOrdered.sort(sortNum)
-
-    //Deixando os tipos iguais para o segundo array
-    stackOrdered.map((stackOrdered) => stackOrdered)
 
     //Comparando valores para pegar os não alterados
     for (let j = 0; j < numCustomers; j++) {
-        if ( stack[j] === stackOrdered[j]) {
+        if ( arriving[j] === stack[j]) {
             stillInPosition++;
         };
     };
