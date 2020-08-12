88be6bcd1c2a28b2d97576a100af2fc47461476f
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2020-08-12 01:07:42 -0300

    Updated files in Filtros em Javascript

---
+++ Ordenação e Filtros em JavaScript/desafio3.js
@@ -0,0 +1,11 @@
+let cases = parseInt(); //Recebe quantidade de casos
+let students = {};
+// Criando uma propriedade
+
+students.name = "mara";
+students.name = "marcos";
+students.name = "mario";
+//students.dict.uniformColor = 'vermelho';
+//students.dict.uniformSize = 'P'
+console.log(students);
+
