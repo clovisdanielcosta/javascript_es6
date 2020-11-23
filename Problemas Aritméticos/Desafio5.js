2740e2c53a9334c7a48efaa150789a948d012877
clovisdanielcosta <clovisdanielcosta@gmail.com>
2020-11-23 05:07:15 -0300

    Added Challenge 5 of 'Problemas Aritméticos'

--- Problemas Aritméticos/Desafio5.js
+++ Problemas Aritméticos/Desafio5.js
@@ -1,19 +1,15 @@
-//######################################
-//###### AINDA EM DESENVOLVIMENTO ######
-//######################################
-
 let ab = gets().split(' ');
 let a = parseInt(ab[0]);
 let b = parseInt(ab[1]);
-let q = a / b;
-let r = a % b;
-let r2;
+let q = 0;
+let r = 0;
 
-if (r < 0) {
- r2 = r + (b * -1);  
- q = (a - r2) / b;
- r = r2;
+for (resto = 0; resto < Math.abs(b); resto++){
+   let quoc = parseInt(((a - resto) / b).toFixed(0));
+   if (a === (b * quoc) + resto) {
+     q = quoc;
+     r = resto;
+   }
 }
 
-console.log(parseInt(q), parseInt(r));
-
+console.log(parseInt(q) + ' ' + parseInt(r));
